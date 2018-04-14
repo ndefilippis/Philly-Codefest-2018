99fbe49adaa719c8274bd8496990425309ef410f
mmaris <m.minea66@gmail.com>
2018-04-14 18:13:58 -0400

    Add signup

--- the-spot-backend/routes.mjs
+++ the-spot-backend/routes.mjs
@@ -18,8 +18,29 @@ app.post('/login', (req, res) => {
     res.send(`userName: ${userName}, hash: ${hash}`);
     // check if password is already in Firebase; if it is, grant permission
     // if password is not in Firebase, don't grant permission but prompt to sign-up
-    // save password in Firebase
   });
 })
+
+//signup
+app.post('/signup', (req, res) => {
+  let name = req.body.name;
+  let birthDate = req.body.birthDate;
+  let sex = req.body.sex;
+  let userName = req.body.userName;
+  let password = req.body.password;
+
+  bcrypt.hash(password, , 3, (err, hash) => {
+    if(err) {
+      res.send(err);
+    }
+    res.send(`name: ${name}, sex: ${sex}, FirthDate: ${birthDate}, userName: ${userName}, hash: ${hash}`);
+
+    //check user name is not taken
+    //check password is complex enough?
+    //log in?
+  });
+  
+}
+//
 
 app.listen(3000);
