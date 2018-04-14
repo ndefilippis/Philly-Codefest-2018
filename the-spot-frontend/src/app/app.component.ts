82ccde81a3e0841670bbc052a1628625e82287c1
Nick DeFilippis <ndefilippis98@gmail.com>
2018-04-14 16:17:31 -0400

    Add google map screen

--- the-spot-frontend/src/app/app.component.ts
+++ the-spot-frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { } from '@types/googlemaps';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
+  ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
 }
