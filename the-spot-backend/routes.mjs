9fcaf1021e3e7824f149031f86f3698979c94bf3
Marius <mariusgarbea@ymail.com>
2018-04-14 17:44:29 -0400

    added backend

---
+++ the-spot-backend/routes.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/login', (req, res) => {
+  let userName = req.body.userName;
+  let password = req.body.password;
+  bcrypt.hash(password, 3, (err, hash) => {
+    if(err) {
+      res.send(err);
+    }
+    res.send(`userName: ${userName}, hash: ${hash}`);
+    // check if password is already in Firebase; if it is, grant permission
+    // if password is not in Firebase, don't grant permission but prompt to sign-up
+    // save password in Firebase
+  });
+})
+
+app.listen(3000);
