7618885aaf54f226f7c9c8206d5c8c76b883e0cd
Nick DeFilippis <ndefilippis98@gmail.com>
2018-04-14 21:25:44 -0400

    Add sign up form layout

--- the-spot-frontend/src/app/signup-screen/signup-screen.component.ts
+++ the-spot-frontend/src/app/signup-screen/signup-screen.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-screen.component.css']
 })
 export class SignupScreenComponent implements OnInit {
-
+  genders = [
+    {value: 'male-0', viewValue: 'Male'},
+    {value: 'female-1', viewValue: 'Female'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ]
   constructor() { }
 
   ngOnInit() {
