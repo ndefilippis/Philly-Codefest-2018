80b911f975192539e363e080210c7324af13c131
Nick DeFilippis <ndefilippis98@gmail.com>
2018-04-15 00:33:43 -0400

    add routing

--- the-spot-frontend/src/app/app-routing.module.ts
+++ the-spot-frontend/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { SignupScreenComponent } from './signup-screen/signup-screen.component';
 
 
 const appRoutes: Routes = [
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'login', component: LoginScreenComponent},
+  {path: 'signup', component: SignupScreenComponent}
 ];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
